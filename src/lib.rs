c8b7793062f6287227480eca8182a8c9d9f33db9
0x736E <281460+0x736E@users.noreply.github.com>
2023-10-31 20:44:11 +0000

    init

---
+++ src/lib.rs
@@ -0,0 +1,148 @@
+use pyo3::prelude::*;
+use pyo3::types::PyList;
+use pest_derive::Parser;
+use pest::Parser;
+use std::collections::HashMap;
+use pest::iterators::Pairs;
+use {
+    once_cell::sync::Lazy,
+    regex::Regex,
+};
+use strum_macros::EnumString;
+use std::str::FromStr;
+
+#[pyclass(get_all)]
+#[derive(Clone, Copy, EnumString)]
+#[strum(ascii_case_insensitive)]
+pub enum Language {
+    Python,
+    Java,
+    JavaScript
+}
+
+#[pyfunction]
+#[allow(dead_code)]
+fn lang_from_str(str_input: &str) -> Language {
+    return Language::from_str(str_input).unwrap();
+}
+
+#[derive(Parser)]
+#[grammar = "JavaScript.pest"]
+pub struct JavaScriptParser;
+
+#[pyclass(get_all)]
+#[derive(Clone, Copy)]
+struct ParseMatch {
+    match_type: String,
+    match_value: String,
+    char: MatchPos,
+    line: MatchPos,
+    matches: HashMap<String, String>
+}
+
+#[pyclass(get_all)]
+#[derive(Clone, Copy)]
+struct MatchPos {
+    start: usize,
+    end: usize
+}
+
+#[pyfunction]
+fn parse_with_enum(py: Python<'_>, str_input: &str, lang: Language) -> PyResult<PyObject> {
+
+    let mut tokens:Vec<PyObject> = Vec::new();
+
+    let pairs: Pairs<Rule> = match lang {
+        Language::JavaScript => JavaScriptParser::parse(Rule::PROGRAM, &str_input).unwrap_or_else(|e| panic!("{}", e)),
+        _=> JavaScriptParser::parse(Rule::PROGRAM, &str_input).unwrap_or_else(|e| panic!("{}", e))
+    };
+
+    for pair in pairs {
+        for inner_pair in pair.into_inner() {
+
+            let rule_str:&str;
+            match inner_pair.as_rule() {
+                Rule::COMMENT => rule_str ="comment",
+                Rule::STRING => rule_str = "string_literal",
+                _=> continue,
+            }
+
+            let p_match = ParseMatch {
+                match_type: rule_str.to_string(),
+                match_value: inner_pair.as_str().to_string(),
+                char: MatchPos {
+                    start: inner_pair.as_span().start_pos().pos(),
+                    end: inner_pair.as_span().end_pos().pos()
+                },
+                line: MatchPos {
+                    start: inner_pair.as_span().start_pos().line_col().0,
+                    end: inner_pair.as_span().end_pos().line_col().0
+                },
+                matches: do_regex(py,  inner_pair.as_str()).into()
+            };
+
+            tokens.push( p_match.into_py(py));
+        }
+    }
+
+    let list: &PyList = PyList::new(py, tokens);
+    Ok(list.into())
+}
+
+#[pyfunction]
+fn parse(py: Python<'_>, str_input: &str, str_lang: &str) -> PyResult<PyObject> {
+    let lang: Language = match Language::from_str(str_lang) {
+        Ok(data) => data,
+        Err(err) => panic!("Problem parsing string: {:?}", err),
+    };
+    return parse_with_enum(py, str_input, lang);
+}
+
+#[allow(dead_code)]
+fn get_patterns() -> HashMap<&'static str, &'static Lazy<Regex>> {
+
+    static RSA_PRIVATE_KEY: Lazy<Regex> = Lazy::new(|| Regex::new(r"(-----BEGIN RSA PRIVATE KEY-----)").unwrap());
+    static SSH_DSA_PRIVATE_KEY: Lazy<Regex> = Lazy::new(|| Regex::new(r"(-----BEGIN DSA PRIVATE KEY-----)").unwrap());
+    static EC_PRIVATE_KEY: Lazy<Regex> = Lazy::new(|| Regex::new(r"(-----BEGIN EC PRIVATE KEY-----)").unwrap());
+    static PGP_PRIVATE_KEY: Lazy<Regex> = Lazy::new(|| Regex::new(r"(-----BEGIN PGP PRIVATE KEY BLOCK-----)").unwrap());
+    static GOOGLE_API_KEY: Lazy<Regex> = Lazy::new(|| Regex::new(r"(AIza[0-9A-Za-z\\-_]{35})").unwrap());
+    static GOOGLE_OAUTH_TOKEN: Lazy<Regex> = Lazy::new(|| Regex::new(r"(ya29\\.[0-9A-Za-z\\-_]+)").unwrap());
+    static GOOGLE_SERVICE_ACCOUNT: Lazy<Regex> = Lazy::new(|| Regex::new("(\"type\": \"service_account\")").unwrap());
+
+    let re_patterns:HashMap<&'static str, &'static Lazy<Regex>> = HashMap::from([
+        ("RSA private key", &RSA_PRIVATE_KEY),
+        ("SSH (DSA) private key", &SSH_DSA_PRIVATE_KEY),
+        ("SSH (EC) private key", &PGP_PRIVATE_KEY),
+        ("PGP private key block", &PGP_PRIVATE_KEY),
+        ("Google API Key", &GOOGLE_API_KEY),
+        ("Google OAuth Access Token", &GOOGLE_OAUTH_TOKEN),
+        ("Google (GCP) Service-account", &GOOGLE_SERVICE_ACCOUNT),
+    ]);
+
+    return re_patterns;
+}
+
+fn do_regex(_py: Python<'_>, str_input: &str) -> HashMap<String, String> {
+
+    let re_patterns: HashMap<&'static str, &'static Lazy<Regex>> = get_patterns();
+    let mut re_captures:HashMap<String, String> = HashMap::new();
+
+    for (key, pattern) in re_patterns {
+        for (cap, [_group1]) in pattern.captures_iter(str_input).map(|c| c.extract()) {
+            re_captures.insert(key.to_string(), cap.to_string());
+        }
+    }
+
+    return re_captures.into();
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn string_extract(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<ParseMatch>()?;
+    m.add_class::<MatchPos>()?;
+    m.add_class::<Language>()?;
+    m.add_function(wrap_pyfunction!(parse_with_enum, m)?)?;
+    m.add_function(wrap_pyfunction!(parse, m)?)?;
+    Ok(())
+}
---
+++ test/test.js
@@ -0,0 +1,43 @@
+
+var myvar = "string"
+
+var myvar = 123;
+
+var myvar = "string with an inline comment no. 1"; /* inline comment for string no. 1*/
+var myvar = "string with an inline comment no. 2"; // inline comment for string no. 2
+
+/* single-line comment */
+
+/*
+    This is a multi-
+    -line comment.
+*/
+
+// single-line comment
+
+var myvar = "string"; // inline-comment
+
+"string \"with\" escaped chars 1"
+
+'string \'with\' escaped chars 2'
+
+"nested 'strings 1' without escapes 3"
+
+'nested \"strings 2\" without escapes 4'
+
+"multiple 'nested' \"strings\" with different enclosure literals"
+
+'multiple \'nested\' \"strings\" with different enclosure literals'
+
+ `this is a single-line string`
+
+var myvar = `this is a single-line string`;
+
+`this is a multi-
+-line string
+using back-ticks`
+
+var myvar = `this is a multi-
+-line string
+using back-ticks`
+
